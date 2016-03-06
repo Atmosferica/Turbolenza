b0047a5c843495b7df57a381448096b50bee74a2
Eugenio Thieme <eugen@thieme.it>
2016-03-06 18:10:05 +0100

    Changes to be committed: 	modified:   main.R 	modified:   turbulence_class.R
    Ho definito un metodo per calcolare la velocità orizzontale relativo alla
    classe "turbulence"... funziona, anche se restituisce un warning (ho capito
    a cosa è dovuto, ma non ho capito bene come ovviare al problema).
    Diciamo che è una prova, sto cercando di capire un attimo come funzionano
    questi S4 (a farlo con gli S3 e UseMethod, come nel cast, non si avrebbero warning).

--- main.R
+++ main.R
@@ -10,5 +10,7 @@ data <- read.csv('./20160129.15r.dat')
 # Converted data (of class data.frame) into an object of class turbulence
 turb <- as.turbulence(data)
 
+hor_velocity <- get_hvel(turb)
+
 # This is only an attempt to make a first main for our project, it should be changed 
 # during the work.
