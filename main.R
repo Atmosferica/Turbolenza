76f3517c378feb0254af90722d37088c89973dd8
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-08 22:01:34 +0100

    Changes to be committed: 	modified:   main.R

--- main.R
+++ main.R
@@ -24,32 +24,33 @@ data <- read.csv('./20160129.15r.dat')
 turb <- as.turbulence(data)
 
 turb <- set_hvel(turb) # for setting horizontal_velocity in turbulence class
-hor_velocity <- get_hvel(turb) 
+hor_vel <- get_hvel(turb)
+hor_velocity <- hor_vel[,1]
 
 
 
 #commento --- e` cambiato il type quindi questa cosa non vale piu`
 
 
-#acq.freq <- 10.
-#Npoint <- length(hor_velocity)
-#time <- Npoint*(1/acq.freq)
-#ts <- seq(0,time,1/acq.freq)
-#f.0 <- 1/time
-#w <- 2*pi*f.0
+acq.freq <- 10.
+Npoint <- length(hor_velocity)
+time <- Npoint*(1/acq.freq)
+ts <- seq(0,time,1/acq.freq)
+f.0 <- 1/time
+w <- 2*pi*f.0
 
-#X.k <- fft(hor_velocity)
-#ampiezze <- Mod(X.k[1:(length(X.k))/2])/Npoint
-#frequenze <- seq(0, acq.freq/2, length.out=length(ampiezze))
-#X.k[20:Npoint] <- 0+0i
-#hvel2 <- Mod(fft(X.k, inverse = TRUE)/(Npoint))
+X.k <- fft(hor_velocity)
+ampiezze <- Mod(X.k[1:(length(X.k))/2])/Npoint
+frequenze <- seq(0, acq.freq/2, length.out=length(ampiezze))
+X.k[20:Npoint] <- 0+0i
+hvel2 <- Mod(fft(X.k, inverse = TRUE)/(Npoint))
 
-#residuals=hor_velocity - hvel2
+residuals=hor_velocity - hvel2
 
 
-#par(mfrow = c(2,2))
-#plot(hor_velocity ~ ts(1:length(hor_velocity)),t='l',xlab="Tempi[s]", ylab="Velocita`[m/s]")
-#plot(hvel2 ~ ts(1:length(hor_velocity)-1), t='l',xlab="Tempi[s]", ylab="Velocita` smooth [m/s]")
-#plot(residuals ~ ts(1:length(hor_velocity)-1), t='l' )
-#plot(ampiezze ~ frequenze, t="l", xlim=c(0,acq.freq/2),log="y", xlab="Frequenze[Hz]", ylab = "Potenza")
+par(mfrow = c(2,2))
+plot(hor_velocity ~ ts(1:length(hor_velocity)-1),t='l',xlab="Tempi[s]", ylab="Velocita`[m/s]")
+plot(hvel2 ~ ts(1:length(hor_velocity)-1), t='l',xlab="Tempi[s]", ylab="Velocita` smooth [m/s]")
+plot(residuals ~ ts(1:length(hor_velocity)-1), t='l' )
+plot(ampiezze ~ frequenze, t="l", xlim=c(0,acq.freq/2),log="y", xlab="Frequenze[Hz]", ylab = "Potenza")
 
