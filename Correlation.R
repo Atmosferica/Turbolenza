d6f287efbecce8aadb5935f61bd194db52bff656
Eugenio Thieme <eugen@thieme.it>
2016-03-18 21:08:18 +0100

    Modifica nomi file
    Per il ciclo generale da fare ho usato sub() con una regex per eliminare il .dat alla fine del nome die file.
    Può essere utile spezzare in due ulteriori parti, per far sì che la prima (giorno di presa dati, aka "nome campagna")
    possa eventualmente venire usata come nome per una cartella da creare automaticamente, l'altra come ora per poter
    essere stampata sui grafici.
    Lavoro appena iniziato, lo finisco appena possibile.

--- Correlation.R
+++ Correlation.R
@@ -6,9 +6,20 @@ library(e1071)
 source('functions.R')
 source('turbulence_class.R')
 
-# Extracted data from csv using the script convert_cvs.awk. 
-data <- read.csv('./20160129.15r.dat')
+var_code <- list.files('.', pattern='*.dat') # listing all the files in the working dir
+var_code <- sub('.dat','', var_code) # removing the '.dat' at the end of the filename
+# It could be useful to separate the name in two parts: the code of the station and the hour
+# (useful for creating directories and putting hour in plots)
 
+
+
+# Extracted data from csv using the script convert_cvs.awk. 
+for(i in 1:length(var_code))
+{
+# data <- read.csv('./20160129.17r.dat')
+
+data <- read.csv(var_code[i])
+  
 # Converted data (of class data.frame) into an object of class turbulence
 turb <- as.turbulence(data)
 
@@ -28,8 +39,42 @@ plot(velZ_T[,1], velZ_T[,2], type="p", pch=20, xlab = "Velocity-Z[m/s]", ylab = 
 	main='Scatterplot vertical velocity vs.  temperature', sub=paste('Correlation: ', round(c2,2), sep=''))
 
 scatplot <- recordPlot()
-print_plot(scatplot, 1200, 900, './grafici_output/scatterplot.png')
+print_plot(scatplot, 1200, 900, paste('./grafici_output/scatterplot_', var_code[i], sep=''))
 rm(scatplot)
+par(mfrow=c(1,1))
+
+# Plotting u and temperature vs. time in the same plot
+u_vel <- get_uvel(turb)
+plot(u_vel[,1], type='l', cex=0.5, xlim=c(0,120), ylim=c(0,15), main='U velocity/temperature vs. time',
+     sub=paste('Correlation uvel/temp: ', cor(u_vel[,1],u_vel[,2]), sep=''))
+points(u_vel[,2], type='l', cex=0.5, col='green')
+#abline(v=47)
+#abline(v=100)
+#abline(v=116)
+legend(1,7,c('u','temp'), c('black','green'))
+cor(u_vel[,1],u_vel[,2])
+
+temp <- recordPlot()
+print_plot(temp, 1200, 900, paste('./grafici_output/xvel_temp_', var_code[i], sep=''))
+
+# Plotting w and temperature vs. time in the same plot
+z_vel <- get_zvel(turb)
+plot(z_vel[,1], type='l', cex=0.5, xlim=c(0,120), ylim=c(-1,15), main='W velocity/temperature vs. time',
+     sub=paste('Correlation w_vel/temp: ', cor(z_vel[,1],z_vel[,2]), sep=''))
+points(z_vel[,2], type='l', cex=0.5, col='green')
+#abline(v=47)
+#abline(v=100)
+#abline(v=116)
+legend(1,7,c('z','temp'), c('black','green'))
+cor(z_vel[,1],z_vel[,2])
+temp <- recordPlot()
+print_plot(temp, 1200, 900, paste('./grafici_output/zvel_temp_', var_code[i], sep=''))
+
+
+
+
+}
+
 
 
 #write(c1,stdout())
