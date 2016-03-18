5e8778a2ca58af6ec95f5a1f86c7d5cb2cf3ef05
Eugenio Thieme <eugen@thieme.it>
2016-03-18 11:16:22 +0100

    Changes to be committed: 	modified:   Periodigram.R 	modified:   turbulence_class.R

--- Periodigram.R
+++ Periodigram.R
@@ -7,7 +7,7 @@ source('functions.R')
 source('turbulence_class.R')
 
 # Extracted data from csv using the script convert_cvs.awk. 
-data <- read.csv('./data/20160129.14r.dat')
+data <- read.csv('20160129.15r.dat')
 
 create_directory('./grafici_output')
 
@@ -19,18 +19,21 @@ vel_T <- get_hvel(turb)
 vel <- vel_T[,1]
 Npoint=length(vel)
 
-# filtered_data <- stft(vel, wtype='hamming.window', coef=500)
-# plot(filtered_data, ylim=c(0,50))
+#hanning <- hanning.window(length(vel))
+#plot(hanning)
 
-# filtered.data <- filter.data(vel,10,20)
-# 
-# par(mfrow = c(2,2))
-# plot(vel ~ filtered.data$tsv,t='l',xlab="Tempi[s]", ylab="Velocita`[m/s]")
-# lines(filtered.data$tsv,filtered.data$hvel2, type='l', col="red", lwd=4)
-# plot(filtered.data$peaks ~ filtered.data$freq, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]",xlim=c(0,1),ylim=c(0,0.03))
-# plot(filtered.data$res ~ filtered.data$tsv, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]")
-# plot(filtered.data$peaks2 ~ filtered.data$freq, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]",xlim=c(0,1),ylim=c(0,0.03))
-# par(mfrow = c(1,1))
+filtered_data <- stft(vel, wtype='hanning.window')
+plot(filtered_data, ylim=c(0,10))
+
+filtered.data <- filter.data(vel,10,20)
+
+par(mfrow = c(2,2))
+plot(vel ~ filtered.data$tsv,t='l',xlab="Tempi[s]", ylab="Velocita`[m/s]")
+lines(filtered.data$tsv,filtered.data$hvel2, type='l', col="red", lwd=4)
+plot(filtered.data$peaks ~ filtered.data$freq, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]",xlim=c(0,1),ylim=c(0,0.03))
+plot(filtered.data$res ~ filtered.data$tsv, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]")
+plot(filtered.data$peaks2 ~ filtered.data$freq, t='l', pch=20, xlab="Tempi[s]", ylab="Velocita`[m/s]",xlim=c(0.001,1),ylim=c(0.001,0.3))#, log=c('x','y'))
+par(mfrow = c(1,1))
 
 # par(mfrow = c(2,2))
 # plot(vel[1:Npoint-1] ~ filtered.data[1:Npoint-1,1],t='l',xlab="Tempi[s]", ylab="Velocita`[m/s]")
