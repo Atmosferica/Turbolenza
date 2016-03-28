74958023b5d4f8e3139e31c5ae075aac9f8adc37
Eugenio Thieme <eugen@thieme.it>
2016-03-28 15:21:59 +0200

    Issue #31: primi passi
    Ho impostato il ciclo da fare sulle fft da fare a diverse frequenze
    di taglio, il lavoro è ancora grezzo e lungi dall'essere finito,
    però la struttura generale potrebbe esserci.
    L'idea è questa: io sono un ricercatore, ho uno script scritto da terzi,
    devo decidere quando tagliare sulla frequenza. A questo punto creo un
    file di testo (io ho messo in /data insieme ai dati convertiti, poi ne
    possiamo discutere) con i tempi e lui va a leggermelo, è più comodo che
    non aprire lo script e andare a modificare il vettore di frequenze nel
    sorgente.
    Siccome a me quei dati mancano, ho messo una decina di tempi di prova,
    poi mettiamo i veri appena li abbiamo.
    I tempi vengono poi convertiti in frequenze, e lì comincia il ciclo.
    Fuori dal ciclo viene creata una cartella all'interno di quella dei grafici
    del singolo dataset in cui vengono sparati tutti i grafici della fft a diverse
    frequenze di taglio, così non si mischiano agli altri (saranno tantini)
    e rimane più facile cercare quello che si vuole.
    
    Problemi aperti:
    - al momento il file viene letto n-mila volte (è nel ciclo delle stazioni):
    può valer la pena spostare la lettura nel main fuori dal ciclo, in modo da
    risparmiare quel decimo di secondo (che sommato ad altri decimi di secondo
    magari fa un bel totale)? Perchè a rigor di logica nel main uno potrebbe anche
    decidere di non eseguire lo script periodigramma (e a quel punto il file è inutile
    importarlo), oppure di passare frequenze diverse per più stazioni (anche se
    non so a che possa servire).
    - al momento abbiamo un filtro passa-basso, noi non volevamo forse anche un passa-banda?
    - temo che i tempi comincino a diventare lunghetti, bisogna rendere efficiente
    al max dove possibile, ed ero quasi tentato dal cercare una soluzione di parallelizzazione
    (però di questo ne parleremo nella milestone "Versione 2" :P)...
    - i grafici sono proprio bruttini, neanche un titolo :D
    
     Changes to be committed:
    	modified:   Periodigram.R
    	modified:   turbulence_class.R

--- Periodigram.R
+++ Periodigram.R
@@ -19,14 +19,32 @@ hamming <- hamming.window(length(vel))
 hamming <- hamming/sum(hamming)*length(vel)
 vel <- vel*hamming
 
-#filtered_data <- stft(vel, wtype='hanning.window')
-#plot(filtered_data, ylim=c(0,10))
+# Importing cut times from file: I think it's better to import from file,
+# because if you want to change the cut threshold you can touch the
+# file instead of source code. 
+# Maybe we can do the call in the main (for avoiding for() loop, little time
+# expensive) or use a save()/load() paradigm...
+cut_times <- read.table(paste(data_path, 'cut_freq', sep=''))
+cut_freq <- 1/cut_times # Converting times into frequencies
 
-par(mfrow=c(2,2))
-data <- dofft(vel,10)
-filt <- filter.data(data$freq,data$fft_vel,0.01)
-plot(data$peaks ~ data$freq, ylim=c(0.001,0.02), xlim=c(0.001,5), type='l',log=c('x','y'))
-plot(filt$peaks ~ filt$freq, ylim=c(0.001,0.02), xlim=c(0.001,5), type='l',log=c('x','y'))
-plot(vel/hamming ~ data$ts, type='l')
-vel_filt=Re(filt$vel)/hamming
-plot(vel_filt ~ data$ts, type='l', ylim=c(-1,1))
+# Here we're running the fft on the array of velocities
+
+for(k in 1:length(cut_freq[,1])){
+  
+  #Creating a dir for the fft-graphs 
+  create_directory(paste(directory_dataset, '/grafici_fft/', sep=''))
+  
+  par(mfrow=c(2, 2))
+  data <- dofft(vel, 10)
+  filt <- filter.data(data$freq, data$fft_vel, cut_freq[k,1])
+  plot(data$peaks ~ data$freq, ylim=c(0.001,0.04), xlim=c(0.001,5), type='l',log=c('x','y'))
+  plot(filt$peaks ~ filt$freq, ylim=c(0.001,0.04), xlim=c(0.001,5), type='l',log=c('x','y'))
+  plot(vel/hamming ~ data$ts, type='l')
+  vel_filt=Re(filt$vel)/hamming
+  plot(vel_filt ~ data$ts, type='l', ylim=c(-1,1))
+  
+  temp <- recordPlot()
+  name <- paste(directory_dataset,"/grafici_fft/fft_cut_",round(cut_freq[k,1], 4),"Hz.png",sep = '')
+  print_plot(temp, 1200, 900, name)
+  
+}
