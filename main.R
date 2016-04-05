592abc2a560c078a306b6c151debfd291f71ef86
Eugenio Thieme <eugen@thieme.it>
2016-04-05 19:29:36 +0200

    Risolto bug grafici + riorganizzazione script
    Ho fatto un po' di cose, basandomi sull'ultima versione del
    branch di Stefano, "Mandelli", al posto che sull'ultima del
    master.
    - ho risolto il bug su plot() segnalato da Stefano quando si
      usa read.csv() con l'opzione header=FALSE. Mi sono accorto
      che, a dispetto di quanto visto oggi in uni, in quelli che
      andavo a stampare automaticamente c'era lo stesso problema.
      Ho riscritto tutti i grafici delle FFT con ggplot() e sembrerebbe
      che il problema non ci sia più. Almeno, a me tutti i grafici
      vengono stampati correttamente.
      Ne ho approfittato per sistemare un po' i titoli e i labels
      degli assi, sono ancora ben lungi dall'essere un mago dei grafici
      ma sono guardabili, se qualcuno vuole migliorarli basta sommare
      il nuovo argomento all'oggetto già esistente.
      IMPORTANTE: che unità di misura hanno i peaks?
    - ho aggiornato il file cut_freq.
    - ho messo il codice (ancora molto grezzo) del metodo degli
      orbitali in uno script a parte (uno potrebbe volerlo eseguire
      senza far necessariamente la FFT).
    - ho fatto sì che dofft() e filter.data() restituiscano un data.frame
      e non più una lista: io non ho riscontrato problemi di compatibilità
      con il codice già scritto, nel caso segnalate e vediamo di fixare.
    
    Con questo vedo se si possono chiudere un paio di issues, intanto mi sembra
    un buon punto per una PR.
    
     Changes to be committed:
    	modified:   Periodigram.R
    	modified:   main.R

--- main.R
+++ main.R
@@ -18,6 +18,8 @@ var_code <- sub('.dat','', filename) # removing the '.dat' at the end of the fil
 filename_tot=paste(data_path,filename, sep="")
 name_dir <- sub('data','grafici_output',sub('.dat','',filename_tot))
 
+create_directory('grafici_output')
+
 for(i in 1:length(filename_tot))
 {
   # Extracted data from csv using the script convert_cvs.awk. 
