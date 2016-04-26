fac757fc4503be6ca28997e5d11b56ebee96ca3d
Stefano Mandelli <linuxfree2@gmail.com>
2016-04-26 22:43:05 +0200

    Better organization of working path

--- main.R
+++ main.R
@@ -12,13 +12,21 @@ source('functions.R')
 source('turbulence_class.R')
 
 
-data_path <- "data/"
+data_path <- path_dir
 filename <- list.files(data_path, pattern='*.dat') # listing all the files in the working dir
 var_code <- sub('.dat','', filename) # removing the '.dat' at the end of the filename
 filename_tot=paste(data_path,filename, sep="")
 name_dir <- sub('data','grafici_output',sub('.dat','',filename_tot))
 
 create_directory('grafici_output')
+create_directory('grafici_output/Fontanella1')
+create_directory('grafici_output/Fontanella2')
+create_directory('grafici_output/Fontanella1/Emission')
+create_directory('grafici_output/Fontanella1/Control')
+create_directory('grafici_output/Fontanella1/LiCor')
+create_directory('grafici_output/Fontanella2/Emission')
+create_directory('grafici_output/Fontanella2/Control')
+create_directory('grafici_output/Fontanella2/LiCor')
 
 for(i in 1:length(filename_tot))
 {
@@ -33,8 +41,14 @@ for(i in 1:length(filename_tot))
   
   create_directory(name_dir[i])
   
+  cat("* Perfoming correlation graph...","\n")
   #source('Correlation.R')
+  
+  cat("* Performing FFT analysis...","\n")
   #source('Periodigram.R')
-  source('orbital_method.R')
+  
+  cat("* Performing Markovian test...","\n")
+  source('markov_test.R')
+  cat("* File: ",name_dir[i],"..done!\n")
 }
 
