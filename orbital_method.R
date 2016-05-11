a5f5d1122b25515954deb89514a7e5dddbdc15b6
Eugenio Thieme <eugen@thieme.it>
2016-05-11 20:06:42 +0200

    Grafici exp
    Quasi finito i grafici degli esponenziali, rimangono un paio di cose da
    vedere (ora comincio ad essere cotto, committo e se non ce la faccio
    stasera finisco domani mattina): mi stampa sempre un solo grafico
    (sospetto che sig non cambi mai, ma al momento mi sembrerebbe invece che
    cicli correttamente...), e ho usato ancora il "vecchio" fit con lm().
    Per ora quello comunque sembra andare discretamente, ho corretto un paio
    di errori nel codice che c'erano, ho compattato la scrittura e tutti gli
    assi dovrebbero essere corretti, con anche i vari parametri come sottotitolo.
    Quello è il modello di grafico corretto (salva anche con nomi diversi nelle
    cartelle giuste), poi si può passare a nls() e la parte dopo comunque si usa
    senza modifiche.
    Vi lascio con una scoperta di oggi, Ibrahim Malouf - True Sorry

--- orbital_method.R
+++ orbital_method.R
@@ -2,81 +2,35 @@
 
 markov_path <- paste(name_dir[i], '/markov', sep='')
 create_directory(markov_path)
-dim_bl <- 5
+dim_bl <- 300
 # Extracting blocks of 5 minutes from original dataset
 
 z_vel <- get_zvel(turb)
 z_vel <- z_vel[,1]
-time_stamp <- seq(from=0, to=length(z_vel)-1)*0.1
-numb <- length(z_vel)%/%(dim_bl*10) # number of blocks: watch out, blocks are in
+time_stamp <- seq(from=0, to=length(z_vel)-1)*(1/sonic_fqc)
+numb <- length(z_vel)%/%(dim_bl*sonic_fqc) # number of blocks: watch out, blocks are in
 # seconds, not in 0.1s...
 for(block in 1:numb){
   sig <- signal.partition(time_stamp, z_vel, block, dim_bl)
-}
+
 
 
   # computing the autocorrelation (we need it for checking the markovianity)
   mark <- autocorr(z_vel, sig)
-
-  # for the exponential fit, fitting the log of the data with lm
-  # plotting the exponential of lm vs. data
-  mark2 <- mark[1:20]
-  exp_fit <- lm(log(mark2) ~ c(1:length(mark2)))
-  model <- exp(predict(exp_fit))
-  plot(mark2, type='p', pch=19, cex=0.5)
-  points(model, type='l')
   
   # plotting the exponential of lm vs. data constraining the intercept to 0
   mark2 <- log(mark[1:20])
   exp_fit <- lm(mark2 ~ c(1:length(mark2))+0)
   model <- exp(predict(exp_fit))
-  plot(exp(mark2), type='p', pch=19, cex=0.5, ylim=c(0,1))
-  points(model, type='l')
-
-
-  # plotting the log of the data vs. lm linear model
-  mark2 <- mark[1:30]
-  exp_fit <- lm(log(mark2) ~ c(1:length(mark2)))
-  model <- predict(exp_fit)
-  plot(log(mark2), type='p', pch=19, cex=0.5)
-  points(model, type='l')
-
-#   # fitting with a 5-th degree polynomial
-#   par(mfrow=c(2,2))
-#   mark2 <- mark[1:30]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 30", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark[1:100]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 100", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark[1:300]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 300", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main=paste("5th degree, npoints = ", length(mark2), sep=''), xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   par(mfrow=c(1,1))
-
+  png(filename=paste(markov_path, '/exponential_fit_zvel_', block,'.png', sep=''), height=400, width=600)
+  plot(exp(mark2), type='p', pch=19, cex=0.5, ylim=c(0,1), main='Exponential
+       fit: z velocity', xlab=paste('Time [', 1/sonic_fqc, 's]', sep=''),
+       ylab='Autocorrelation', sub=paste('Coeff: ', round(summary(exp_fit)$coefficients[1,1],2),
+                                         ' R^2: ', round(summary(exp_fit)$r.squared,2),
+                                         ' Decorrelation time:', abs(round((1/summary(exp_fit)$coefficients[1,1]), 2)), sep=''))
+  points(model, type='l', col='red')
+  dev.off()
+}
 
 # ******************************************************
 # ************* HORIZONTAL VELOCITY ********************
@@ -88,114 +42,27 @@ for(block in 1:numb){
 turb <- set_hvel(turb)
 h_vel <- get_hvel(turb)
 h_vel <- h_vel[,1]
-time_stamp <- seq(from=0, to=length(h_vel)-1)*0.1
-numb <- length(h_vel)%/%3000 # number of blocks: watch out, blocks are in
+time_stamp <- seq(from=0, to=length(h_vel)-1)*(1/sonic_fqc)
+numb <- length(h_vel)%/%(dim_bl*sonic_fqc) # number of blocks: watch out, blocks are in
 # seconds, not in 0.1s...
 for(block in 1:numb){
-  sig <- signal.partition(time_stamp, h_vel, block, 300)
+  sig <- signal.partition(time_stamp, h_vel, block, dim_bl)
 
 
   # computing the autocorrelation (we need it for checking the markovianity)
   mark <- autocorr(h_vel, sig)
 
-  # for the exponential fit, fitting the log of the data with lm
-  # plotting the exponential of lm vs. data
-  mark2 <- mark[1:30]
-  exp_fit <- lm(log(mark2) ~ c(1:length(mark2)))
-  model <- exp(predict(exp_fit))
-  plot(mark2, type='p', pch=19, cex=0.5)
-  points(model, type='l')
-
-
+  # plotting the exponential of lm vs. data constraining the intercept to 0
   mark2 <- log(mark[1:20])
   exp_fit <- lm(mark2 ~ c(1:length(mark2))+0)
   model <- exp(predict(exp_fit))
-  plot(exp(mark2), type='p', pch=19, cex=0.5)
-  points(model, type='l')
-
-  # plotting the log of the data vs. lm linear model
-  mark2 <- mark[1:30]
-  exp_fit <- lm(log(mark2) ~ c(1:length(mark2)))
-  model <- predict(exp_fit)
-  plot(log(mark2), type='p', pch=19, cex=0.5)
-  points(model, type='l')
-
-#   # fitting with a 5-th degree polynomial
-#   par(mfrow=c(2,2))
-#   mark2 <- mark[1:30]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 30", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark[1:100]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 100", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark[1:300]
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main="5th degree, npoints = 300", xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   mark2 <- mark
-#   predic <- c(1:length(mark2))
-#   pol_fit <- lm(mark2 ~ poly(predic, 5))
-#   model <- predict(pol_fit)
-#   plot(mark2, type='p', pch=19, cex=0.2, main=paste("5th degree, npoints = ", length(mark2), sep=''), xlab="Time [10^(-1)s]",
-#        ylab="Correlation")
-#   points(model, type='l', col='red', lwd=1.3)
-# 
-#   par(mfrow=c(1,1))
-
-}
-
-
-
-# Finding autocorrelation for horizontal velocity
-# 
-# hor_vel <- get_hvel(turb)
-# x <- hor_vel[,1]
-# x.a <- x[-length(x)] # deleting last item
-# x.b <- x[-1] # deleting first item
-# rm(x)
-# x <- as.data.frame(cbind(x.a, x.b)) # ggplot needs a data.frame
-# # smoothScatter(x.a, x.b)
-# correl <- cor(x.a, x.b)
-# g1 <- ggplot(data=x, aes(x=x.a, y=x.b)) + ggtitle('Autocorrelation for horizontal velocity') +
-#       stat_density2d(geom="tile", aes(fill=..density..^0.25), contour=F) +
-#       scale_fill_gradientn(colours = colorRampPalette(c("white", blues9))(256)) +
-#       xlab('Hor_vel without last item') + ylab('Hor_vel without first item') +
-#       annotate('text', x=155, y=15, label=paste('Correlation: ', round(correl, 2), sep=''), )
-# #g1
-# ggsave(paste(markov_path, '/autocorrelation_hvel.png', sep=''))
-# rm(g1)
-
-# The same thing for vertical velocity
-
-# z_vel <- get_zvel(turb)
-# z <- z_vel[,1]
-# z.a <- z[-length(z)] # deleting last item
-# z.b <- z[-1] # deleting first item
-# # rm(z)
-# z <- as.data.frame(cbind(z.a, z.b)) # ggplot needs a data.frame
-# correl <- cor(z.a, z.b)
-# g1 <- ggplot(data=z, aes(x=z.a, y=z.b)) + ggtitle('Autocorrelation for vertical velocity') +
-#   stat_density2d(geom="tile", aes(fill=..density..^0.25), contour=F) +
-#   scale_fill_gradientn(colours = colorRampPalette(c("white", blues9))(256)) +
-#   xlab('Hor_vel without last item') + ylab('Hor_vel without first item') +
-#   annotate('text', x=70, y=5, label=paste('Correlation: ', round(correl, 2), sep=''), )
-# #g1
-# ggsave(paste(markov_path, '/autocorrelation_zvel.png', sep=''))
-# rm(g1)
-
-
+  png(filename=paste(markov_path, '/exponential_fit_hvel_', block, '.png', sep=''), height=400, width=600)
+  plot(exp(mark2), type='p', pch=19, cex=0.5, ylim=c(0,1), main='Exponential
+       fit: h velocity', xlab=paste('Time [', 1/sonic_fqc, 's]', sep=''),
+       ylab='Autocorrelation', sub=paste('Coeff: ', round(summary(exp_fit)$coefficients[1,1],2),
+                                         ' R^2: ', round(summary(exp_fit)$r.squared,2),
+                                         ' Decorrelation time:', abs(round((1/summary(exp_fit)$coefficients[1,1]), 2)), sep=''))
+  points(model, type='l', col='red')
+  dev.off()
+} 
 
