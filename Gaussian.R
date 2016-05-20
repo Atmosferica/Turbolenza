343488510f3fbb45e70be1581ea3a245e5c5fc10
chiaradf <chia.df@gmail.com>
2016-05-20 15:58:53 +0200

    Per Anna

--- Gaussian.R
+++ Gaussian.R
@@ -50,8 +50,8 @@ for(fl in 1:length(filename_dati_tot))
               path_output_new<-paste(path_output, "block/", sep ='')
               create_directory(path_output_new)
              
-              f_cut_up <- 0.015
-              f_cut_down <- 0.005
+              f_cut_up <- 0.1
+              f_cut_down <- 0.05
          
              ##Finding kurtosis-skewness and mean-sd for x-velocity.
              ##firth column: date; second : hour
@@ -226,8 +226,8 @@ for(fl in 1:length(filename_dati_tot))
           x_vel <- get_uvel(turb)
           x <- x_vel[,1]  
           
-          f_cut_up <- 0.015
-          f_cut_down <- 0.005
+          f_cut_up <- 0.1
+          f_cut_down <- 0.05
           
           hamming <- hamming.window(length(x))
           hamming <- hamming/sum(hamming)*length(x)
--- main.R
+++ main.R
@@ -85,7 +85,7 @@ filename_dati_tot=paste(data_path,filename_dati, sep="")
 # dev.off()
 
 cat("* Perfoming Gaussian con filtro...","\n")
-#source('Gaussian.R')
+source('Gaussian.R')
 
 cat("* Perfoming Gaussian senza filtro...","\n")
 source('senza_filtro.R')
